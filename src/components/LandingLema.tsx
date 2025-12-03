f0b9a180f0865e89c165b2b25518092854cdde75
manel-castro <manel.castro39@gmail.com>
2025-12-03 11:49:10 +0100

    linear gradient

--- src/components/LandingLema.tsx
+++ src/components/LandingLema.tsx
@@ -57,9 +57,8 @@ const LandingLema: React.FC = () => {
           paddingTop: "4rem",
           paddingBottom: "4rem",
           ...padding,
-          backgroundColor: "#000000",
           background:
-            "linear-gradient(to right, rgba(0,0,0,1) 0%, rgba(0,0,0,1) calc(50% - 1px), rgba(0,0,0,0.99) 52%, rgba(0,0,0,0.75) 62%, rgba(0,0,0,0.5) 74%, rgba(0,0,0,0.25) 88%, rgba(0,0,0,0) 100%)",
+            "linear-gradient(to right, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 50%, rgba(0,0,0,0.99) 52%, rgba(0,0,0,0.95) 58%, rgba(0,0,0,0.5) 74%, rgba(0,0,0,0.25) 88%, rgba(0,0,0,0) 100%)",
         }}
       >
         <h1
@@ -92,19 +91,6 @@ const LandingLema: React.FC = () => {
         >
           {t("landing.learnMore")}
         </Button>
-        {/* vertical divider at the midpoint to make the boundary visible */}
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            bottom: 0,
-            left: "50%",
-            width: "1px",
-            backgroundColor: "rgba(255,255,255,0.06)",
-            zIndex: 3,
-            pointerEvents: "none",
-          }}
-        />
       </div>
     </div>
   );
